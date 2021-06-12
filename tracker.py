4bed83bb60ffc347b52312a72ab7c1a5b18954d7
Zoplos <giannis97z97z@gmail.com>
2021-06-12 17:30:03 +0300

    initial commit

---
+++ tracker.py
@@ -0,0 +1,43 @@
+from flask import Flask,render_template,request,redirect
+from markupsafe import escape
+import numpy as np
+import operator
+
+app = Flask(__name__)
+inputs = []
+class Character:
+    def __init__(self,roll,name,hp,ac):
+        self.roll = roll
+        self.name = name
+        self.hp = hp
+        self.ac = ac  
+
+@app.route('/')
+def index():
+    return render_template("base.html") 
+
+@app.route("/hello")
+@app.route("/hello/<name>")
+def hello(name=None):
+    return render_template('hello.html',name=name)
+
+@app.route('/data/', methods = ['POST', 'GET'])
+def data():
+    if request.method == 'POST':
+        # form_data = request.form
+        roll = int(request.form['init'])
+        name = request.form['name']
+        hp = int(request.form['hp'])
+        ac = int(request.form['ac'])
+        inputs.append(Character(roll,name,hp,ac))
+        sorted_inputs = sorted(inputs, key=operator.attrgetter("roll"))
+        sorted_inputs = reversed(sorted_inputs)
+        # return render_template('data.html',form_data=form_data)
+        return render_template('data.html',inputs=sorted_inputs)
+
+@app.route('/clear', methods = ['POST', 'GET'])
+def clear():
+    inputs.clear()
+    return render_template("base.html") 
+        
+
